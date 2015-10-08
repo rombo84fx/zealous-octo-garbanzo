2bf247125684483c234988ca8842b7eb46aa2e58
Gerardo Martinez <gmart_nez@yahoo.com.mx>
2015-10-08 16:57:39 -0500

    Module Three Assigment First Version

--- ModuleThreeAssignment/ModuleThreeAssignment/Program.cs
+++ ModuleThreeAssignment/ModuleThreeAssignment/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ModuleThreeAssignment
 {
@@ -10,6 +6,49 @@ namespace ModuleThreeAssignment
     {
         static void Main(string[] args)
         {
+            //// Student Attributes
+            //string studentFirstName;
+            //string studentLastName;
+            //DateTime studentBirthDate;
+
+            //// Teacher Attributes
+            //string teacherFirstName;
+            //string teacherLastName;
+            //DateTime teacherBirthDate;
+
+            //// Degree Attributes
+            //string degreeName;
+            //string degreeDescription;
+            //string degreeType;
+
+            //// Program Attributes
+            //string programName;
+            //string programDescription;
+            //string programSchool;
+
+            //// Course Attributes
+            //string courseName;
+            //string courseDescription;
+            //string courseInstructor;
+
+            GetStudentInformation();
+            Console.ReadKey();
+        }
+
+        static void GetStudentInformation()
+        {
+            Console.WriteLine("Enter the student's first name: ");
+            string firstName = Console.ReadLine();
+            Console.WriteLine("Enter the student's last name: ");
+            string lastName = Console.ReadLine();
+            Console.WriteLine("Enter the student's birth date:");
+            DateTime birthDate = DateTime.Parse(Console.ReadLine());
+            PrintStudentDetails(firstName, lastName, birthDate.ToString("d"));
+        }
+
+        static void PrintStudentDetails(string first, string last, string birthday)
+        {
+            Console.WriteLine("{0} {1} was born on: {2}", first, last, birthday);
         }
     }
 }
