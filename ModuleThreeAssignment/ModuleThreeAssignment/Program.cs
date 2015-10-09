67d9c16216d0b6d3324b17a2d68ac3191dacd418
Gerardo Martinez <gmart_nez@yahoo.com.mx>
2015-10-09 16:49:06 -0500

    Second version of module three assignment

--- ModuleThreeAssignment/ModuleThreeAssignment/Program.cs
+++ ModuleThreeAssignment/ModuleThreeAssignment/Program.cs
@@ -6,16 +6,6 @@ namespace ModuleThreeAssignment
     {
         static void Main(string[] args)
         {
-            //// Student Attributes
-            //string studentFirstName;
-            //string studentLastName;
-            //DateTime studentBirthDate;
-
-            //// Teacher Attributes
-            //string teacherFirstName;
-            //string teacherLastName;
-            //DateTime teacherBirthDate;
-
             //// Degree Attributes
             //string degreeName;
             //string degreeDescription;
@@ -32,6 +22,7 @@ namespace ModuleThreeAssignment
             //string courseInstructor;
 
             GetStudentInformation();
+            validateDate(DateTime.Parse("05/07/1984"));
             Console.ReadKey();
         }
 
@@ -48,7 +39,69 @@ namespace ModuleThreeAssignment
 
         static void PrintStudentDetails(string first, string last, string birthday)
         {
-            Console.WriteLine("{0} {1} was born on: {2}", first, last, birthday);
+            Console.WriteLine("The student {0} {1} was born on: {2}", first, last, birthday);
+        }
+
+        static void GetTeacherInformation()
+        {
+            Console.WriteLine("Enter the teacher's first name: ");
+            string firstName = Console.ReadLine();
+            Console.WriteLine("Enter the teacher's last name: ");
+            string lastName = Console.ReadLine();
+            Console.WriteLine("Enter the teacher's birth date: ");
+            DateTime birthDate = DateTime.Parse(Console.ReadLine());
+            PrintTeacherDetails(firstName, lastName, birthDate.ToString());
+        }
+
+        static void PrintTeacherDetails(string first, string last, string birthday)
+        {
+            Console.WriteLine("The teacher {0} {1} was born on: {2}", first, last, birthday);
+        }
+
+        static void GetDegreeInformation()
+        {
+            Console.WriteLine("Enter the degree's name: ");
+            string degreeName = Console.ReadLine();
+            Console.WriteLine("Enter the degree's description: ");
+            string degreeDescription = Console.ReadLine();
+            Console.WriteLine("Enter the degree's type: ");
+            string degreeType = Console.ReadLine();
+            PrintDegreeInformation(degreeName, degreeDescription, degreeType);
+        }
+
+        static void PrintDegreeInformation(string name, string description, string type)
+        {
+            Console.WriteLine("The degree {0} is {1} and has a type of {2}", name, description, type);
+        }
+
+        static void GetProgramInformation()
+        {
+            Console.WriteLine("Enter the program's name: ");
+            string programName = Console.ReadLine();
+            Console.WriteLine("Enter the program's description: ");
+            string programDescription = Console.ReadLine();
+            Console.WriteLine("Enter the program's school: ");
+            string programSchool = Console.ReadLine();
+            PrintProgramInformation(programName, programDescription, programSchool);
+        }
+
+        static void PrintProgramInformation(string name, string description, string school)
+        {
+            Console.WriteLine("The school of {0} has the {1} program, that is {2}", school, name, description);
+        }
+
+        static void GetCourseInformation()
+        {
+            Console.WriteLine("Enter the course's name");
+        }
+        static string validateDate(DateTime date)
+        {
+            throw new NotImplementedException();
+        }
+
+        static string validateDateChallenge(DateTime date)
+        {
+
         }
     }
 }
